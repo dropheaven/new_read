0080bf48f7c5d4131321c684ac49957f9e56b4f0
Saad <Dropheaven@live.com>
2017-05-11 11:45:09 -0400

    added class method top_100_novels. At the moment it is only returning the HTML code that makes up good reads Time Magazine's All-Time 100 Novels.

--- lib/new_read/scraper.rb
+++ lib/new_read/scraper.rb
@@ -2,5 +2,8 @@ require 'open-uri'
 require 'pry'
 
 class Scraper
+  def self_top_100_novels
+    doc = Nokogiri::HTML(open(http://www.goodreads.com/list/show/2681.Time_Magazine_s_All_Time_100_Novels))
 
+  end
 end
